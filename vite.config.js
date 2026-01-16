0344a6f094ed43eeaafd4a54e9d82aef880765b8
sebas2906 <s.sebas33@gmail.com>
2026-01-16 14:54:57 -0500

    Agregado allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default {
     server:
     {
         host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
+        allowedHosts: true
     },
     build:
     {
